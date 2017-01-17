cf92dbeb0d7119e3b4eeab0e2f903af721b52e34
eater <hello@eaterofco.de>
2017-01-17 18:52:04 +0100

    Fix run.sh for travis-ci

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e;
 
-cd "$(realpath "$(dirname "$0")")/../"
+cd "$(readlink -f -- "$(dirname "$0")")/../"
 
 truncate -s0 test/db.sqlite3;
-composer dump-autoload;
+composer dump-autoload || true;
 ./vendor/bin/propel model:build;
 ./vendor/bin/propel config:convert --config-dir test --output-file=test-config.php
 ./vendor/bin/propel sql:build --overwrite --config-dir test --output-dir test/data/sql;
